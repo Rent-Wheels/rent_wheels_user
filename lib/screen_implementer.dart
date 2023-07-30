c074dc141ffff9a99f2fa381ca956dba29f08195
bnakusaki <akusakibenjaminnartey@gmail.com>
2023-07-30 13:27:09 +0000

    demo screen

--- lib/screen_implementer.dart
+++ lib/screen_implementer.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:rent_wheels/core/backend/reservations/methods/reservations_methods.dart';
+import 'package:rent_wheels/core/models/reservations/reservations_model.dart';
+import 'package:rent_wheels/core/widgets/buttons/adaptive_back_button_widget.dart';
 import 'package:rent_wheels/core/widgets/sizes/sizes.dart';
 import 'package:rent_wheels/core/widgets/spacing/spacing.dart';
 import 'package:rent_wheels/core/widgets/textStyles/text_styles.dart';
 import 'package:rent_wheels/core/widgets/theme/colors.dart';
+import 'package:rent_wheels/src/mainSection/reservations/widgets/horizontal_scroll_list.dart';
 
 import 'src/mainSection/reservations/data/reservations_data.dart';
 
@@ -14,33 +18,33 @@ class ReservationsPageMock extends StatefulWidget {
 }
 
 class _ReservationsPageMockState extends State<ReservationsPageMock> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: rentWheelsNeutralLight0,
-      appBar: AppBar(
-        backgroundColor: rentWheelsNeutralLight0,
-        elevation: 0,
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding:
-              EdgeInsets.symmetric(horizontal: Sizes().width(context, 0.04)),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                "Reservations",
-                style: heading3Information,
-              ),
-              Space().height(context, 0.03),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding:
+                EdgeInsets.symmetric(horizontal: Sizes().width(context, 0.04)),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                buildAdaptiveBackButton(onPressed: (){}),
+                Space().height(context, 0.02),
+                const Text("Reservations", style: heading3Information),
+                Space().height(context, 0.02),
 
-              // Reservation filters
-              // TODO: Add the horizontal scroll list for the reservation filters.
+                // Reservation filters
+                // TODO: Add the horizontal scroll list for the reservation filters.
+                const HorizontalScroll(),
+                Space().height(context, 0.02),
 
-              // Reservation Data
-              const ReservationsData(),
-            ],
+                // Reservation Data
+                const ReservationsData(),
+              ],
+            ),
           ),
         ),
       ),
