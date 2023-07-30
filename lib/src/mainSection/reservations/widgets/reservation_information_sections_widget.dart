65bbe8cdd004f9b1ce3b9654919f5b4b9adc8e4e
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-07-30 10:44:28 +0000

    changes: minor changes

--- lib/src/mainSection/reservations/widgets/reservation_information_sections_widget.dart
+++ lib/src/mainSection/reservations/widgets/reservation_information_sections_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:rent_wheels/core/models/cars/cars_model.dart';
 import 'package:rent_wheels/core/models/reservations/reservations_model.dart';
 import 'package:rent_wheels/src/mainSection/reservations/widgets/car_image_widget.dart';
