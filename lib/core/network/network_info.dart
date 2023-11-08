86e80df77e48d078db5fc0fccacbeaefecee639c
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-11-08 20:25:45 +0000

    add: usecase abstract class and network checker

---
+++ lib/core/network/network_info.dart
@@ -0,0 +1,14 @@
+import 'package:data_connection_checker_nulls/data_connection_checker_nulls.dart';
+
+abstract class NetworkInfo {
+  Future<bool> get isConnected;
+}
+
+class NetworkInfoImpl extends NetworkInfo {
+  final DataConnectionChecker dataConnectionChecker;
+
+  NetworkInfoImpl({required this.dataConnectionChecker});
+
+  @override
+  Future<bool> get isConnected => dataConnectionChecker.hasConnection;
+}
