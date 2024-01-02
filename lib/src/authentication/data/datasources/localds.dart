1f41eb753ba0595ad35c636f9ad92386027b3c8a
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2024-01-02 07:39:03 +0000

    rf: backend local datasource impl changed to authentication localds

---
+++ lib/src/authentication/data/datasources/localds.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+
+import 'package:rent_wheels/src/authentication/data/datasources/backend/localds.dart';
+import 'package:rent_wheels/src/user/data/model/user_info_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AuthenticationLocalDatasource
+    implements BackendAuthenticationLocalDatasource {
+  final SharedPreferences sharedPreferences;
+  final String cacheKey = 'USER_CACHE_KEY';
+
+  AuthenticationLocalDatasource({required this.sharedPreferences});
+
+  @override
+  Future cacheUserInfo(BackendUserInfoModel userInfo) {
+    final cacheData = jsonEncode(userInfo.toJson());
+    return sharedPreferences.setString(cacheKey, cacheData);
+  }
+
+  @override
+  Future deleteCachedUserInfo() {
+    return sharedPreferences.remove(cacheKey);
+  }
+
+  @override
+  Future<BackendUserInfoModel> getCachedUserInfo() async {
+    final cacheData = sharedPreferences.getString(cacheKey);
+
+    if (cacheData == null) {
+      throw Exception('Cache error');
+    }
+
+    return BackendUserInfoModel.fromJSON(json.decode(cacheData));
+  }
+}
